283c9eaf0d6502848c2d7c598f11c7c24c0cda3b
MitchelHsu <mitchel101350@gmail.com>
2022-06-14 20:58:09 +0800

    improve Galaxy

--- static/js/Galaxy.js
+++ static/js/Galaxy.js
@@ -45,6 +45,18 @@ let pageData = {
     ]
 };
 
+$(function() {
+    $(".trigger_popup_fricc").on('click', function(){
+        $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').on('click', function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').on('click', function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+
 // const app = Vue.createApp({
 //     data(){
 //         return pageData;
